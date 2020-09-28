c93144332e9f450bef1a4a7cea5ac0ac52dbfd61
bilal-wsib <bilal@wsib-innovation.ca>
2020-09-28 08:46:26 -0400

    Updates

---
+++ precache-manifest.e7db2f930519660a3f972da16dbc8813.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7ae012ae203be227a1e7930c43272ea",
+    "url": "/actionsTest/index.html"
+  },
+  {
+    "revision": "f5faeaf178297bab70e1",
+    "url": "/actionsTest/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "49ed26918e1a27ac9e51",
+    "url": "/actionsTest/static/js/2.2971500b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/actionsTest/static/js/2.2971500b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5faeaf178297bab70e1",
+    "url": "/actionsTest/static/js/main.2daf92b6.chunk.js"
+  },
+  {
+    "revision": "a5ef167624afd877008a",
+    "url": "/actionsTest/static/js/runtime-main.0c45021f.js"
+  }
+]);
